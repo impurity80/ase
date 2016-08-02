df876b4dab4ed42eb95df1459443bc4600a89385
impurity <impurity@postech.ac.kr>
2016-08-02 19:31:47 +0900

    first

--- proj02/NEB/neb.py
+++ proj02/NEB/neb.py
@@ -10,7 +10,7 @@ from ase.utils.eos import EquationOfState
 from ase.lattice.cubic import BodyCenteredCubic
 from ase import Atom
 
-a = 2.87
+a = 2.8920
 
 cell = [[1,0,0],[0,1,0],[0,0,1]]
 atoms = BodyCenteredCubic('Fe', directions=cell)
@@ -19,6 +19,10 @@ atoms.set_initial_magnetic_moments([5,5])
 carbon = Atom('C', position=(0,0.5*a,0.5*a), charge=0.4)
 
 atoms = atoms*(2,2,2) + carbon
+init = atoms.copy()
+
+final = atoms.copy()
+final[16].position = [0, 0.5*a, (0.5+0.5)*a]
 
 for i in range(11):
     os.system('mkdir {:02d}'.format(i))
@@ -28,3 +32,12 @@ for i in range(11):
     write('POSCAR', atoms)
     os.chdir('..')
 
+view(init)
+view(final)
+
+def save( filename, arg ):
+    f = open(filename, 'a+t')
+    f.write('{0} \n'.format(arg))
+    f.close()
+
+os.system('mkdir result')
---
+++ proj02/NEB/step1.py
@@ -0,0 +1,95 @@
+from neb import *
+
+id = 1
+
+curr_dir = os.getcwd()
+work_dir = 'work-{0}'.format(id)
+result_file = '{0}/result/result_{1}.txt'.format(curr_dir,id)
+
+os.system('mkdir {0}'.format(work_dir))
+os.system('rm -f {0}'.format(result_file))
+save(result_file, atoms.get_positions())
+
+os.chdir(work_dir)
+
+OPTIONS = ['init','final']
+energies = []
+
+for opt in OPTIONS:
+
+    save(result_file, 'Option : {0}'.format(opt))
+
+    if opt=='init':
+        atoms = init
+    elif opt=='final':
+        atoms = final
+
+    dir = 'opt-{0}'.format(opt)
+    os.system('mkdir {0}'.format(dir))
+    os.chdir('{0}'.format(dir))
+
+    calc = Vasp(istart = 0,
+                icharg = 2,
+                xc = 'PBE',
+                kpts = (1,1,1),
+                encut = 720,
+                ispin = 2,
+                setups = {'Fe':'_pv'},
+                ismear = 1,
+                sigma = 0.1,
+          #      prec='Accurate',
+          #      ediff=0.00025,
+                algo='Fast',
+                potim=0.05,
+                lorbit=11,
+                lreal='False',
+                lwave=False,
+                nelm=100
+                )
+
+    atoms.set_calculator(calc)
+    p = atoms.get_potential_energy()
+    print p, p/len(atoms)
+
+    save(result_file, '{0} {1}'.format(p, p/len(atoms)))
+
+    # k-point increase
+    calc.set(istart = 1,
+             icharg = 0,
+             kpts=(8,8,8)
+             )
+    p = atoms.get_potential_energy()
+
+    save(result_file, '{0} {1}'.format(p, p/len(atoms)))
+    save(result_file, '{0}'.format(atoms.get_forces()))
+
+    calc.set(isif=2,ibrion=2, nsw=99)
+    p = atoms.get_potential_energy()
+    m = atoms.get_magnetic_moment()
+    v = atoms.get_volume()
+
+    energies.append(p)
+
+    save(result_file, '3rd calculation \n total energy : {0} eV, {1} eV/atom'.format(p, p/len(atoms)))
+    save(result_file, 'magnetic moment : {0} mB, {1} mB/atom'.format(m, m/len(atoms)))
+    save(result_file, 'forces \n {0}'.format(atoms.get_forces()))
+
+    save(result_file, '----------------------')
+
+    print p, p/len(atoms)
+    print atoms.get_forces()
+
+    os.chdir('..')
+
+os.chdir('..')
+
+save(result_file, '{0}'.format(OPTIONS))
+save(result_file, '{0}'.format(energies))
+save(result_file, '----------------------')
+
+plt.plot(OPTIONS, energies)
+plt.xlabel('OPTIONS')
+plt.ylabel('Energy (eV)')
+
+plt.savefig('{0}/result/step{1}.png'.format(curr_dir, id))
+plt.show('{0}/result/step{1}.png'.format(curr_dir, id))
