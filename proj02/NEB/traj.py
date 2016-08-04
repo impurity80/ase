16cec1a2040bda815888db4e4625e7a26f5d19f7
impurity <impurity@postech.ac.kr>
2016-08-04 19:40:02 +0900

    diffusion

--- proj02/NEB/traj.py
+++ proj02/NEB/traj.py
@@ -26,7 +26,7 @@ init = atoms.copy()
 final = atoms.copy()
 final[-1].position = [0, 0.5*a, (0.5+1.0)*a]
 
-images = []
+images = [init]
 
 for i in range(9):
     os.system('mkdir 0{0}'.format(i+1))
@@ -37,6 +37,8 @@ for i in range(9):
     images.append(atoms.copy())
  #   write('POSCAR', atoms)
     os.chdir('..')
+
+images.append(final)
 
 #view(init)
 #view(final)
