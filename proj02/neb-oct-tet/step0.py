4a2d01673956240c68356f9d0cbbd55bc840984b
impurity <impurity@postech.ac.kr>
2016-08-05 16:08:27 +0900

    diffusion

--- proj02/neb-oct-tet/step0.py
+++ proj02/neb-oct-tet/step0.py
@@ -15,6 +15,7 @@ a = 2.8920
 cell = [[1,0,0],[0,1,0],[0,0,1]]
 atoms = BodyCenteredCubic('Fe', directions=cell)
 atoms.set_initial_magnetic_moments([5,5])
+atoms.set_cell([a,a,a], scale_atoms=True)
 
 carbon = Atom('C', position=(0,0.5*a,0.5*a), charge=0.4)
 
